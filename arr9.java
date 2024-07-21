81a7ca79d37dbece9b5b4314e9f6fb79ef0c787a
swapnali2503 <swapnaliwandhekar25@gmail.com>
2024-07-22 00:19:07 +0530

    Add files via upload

---
+++ arr9.java
@@ -0,0 +1,51 @@
+/* sort array in ascending order */
+
+
+import java.util.*;
+
+public class arr9 {
+    
+    public static void printArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void sortArray(int[] array) {
+        int temporary;
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[i] > array[j]) {
+                    temporary = array[i];
+                    array[i] = array[j];
+                    array[j] = temporary;
+                }
+            }
+        }
+        System.out.println("Elements of array in ascending order:");
+        printArray(array);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = sc.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        System.out.println("Elements of original array:");
+        printArray(array);
+
+        sortArray(array);
+    }
+}
+
+
+    
+
