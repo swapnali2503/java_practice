a559befac563baee8be549e7eed37a3c598a4566
swapnali2503 <swapnaliwandhekar25@gmail.com>
2024-07-22 23:46:28 +0530

    Add files via upload

---
+++ arr11.java
@@ -0,0 +1,30 @@
+
+
+import java.util.*;
+public class arr11{
+    public static void main(String[] args){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter size of array:");
+        int n = sc.nextInt();
+
+        int arr[] =  new int[n];
+        System.out.println("enter elements of array:");
+        for(int i =0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int max = arr[0];
+        for(int i =0;i<n;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+
+        System.out.println("maximum number is:"+ max);
+    }
+
+ 
+
+}
+    
+
